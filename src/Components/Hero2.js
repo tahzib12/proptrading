2aaccbedaeec67fe92d0754ade98061324131da8
Himans <himans.codeskape@gmail.com>
2024-09-08 12:44:23 +0530

    motion duration

--- src/Components/Hero2.js
+++ src/Components/Hero2.js
@@ -5,7 +5,7 @@ import 'aos/dist/aos.css'
 const Hero2 = () => {
   useEffect(() => {
     AOS.init({
-      duration: 700, // Animation duration (1000ms = 1s)
+      duration: 1000, // Animation duration (1000ms = 1s)
       //   offset: 200, // Offset (distance in pixels) from the original trigger point
       once: false, // Whether the animation should happen only once
       easing: 'ease', // Easing function for the animation
@@ -16,32 +16,32 @@ const Hero2 = () => {
 
   return (
     <div className="bg-[rgba(22,5,48,1)] text-center py-16 max-w-full h-[600px]">
-      <h1 className="text-white text-[60px] sm:text-[30px] md:text-[35px] font-bold " data-aos="fade-up">
+      <h1 className="text-white text-[60px] sm:text-[30px] md:text-[35px] font-bold " data-aos="fade-up" data-aos-delay="1000">
         OUR SIMPLE PROVEN PROCESS
       </h1>
       <div className='flex justify-between mx-auto max-w-[90%] gap-3 mt-20'>
-        <div className='flex flex-col bg-purple-800 rounded-3xl max-w-[300px] p-4' data-aos="fade-down">
-          <span className='text-black text-[25px] font-bold'>Step 1 :</span>
-          <span className='text-black text-[25px] font-bold'>Evaluation</span>
+        <div className='flex flex-col bg-purple-800 rounded-3xl max-w-[300px] p-4' data-aos="fade-down" data-aos-delay="1500">
+          <span className='text-white text-[25px] font-bold'>Step 1 :</span>
+          <span className='text-white text-[25px] font-bold'>Evaluation</span>
           <span className='text-white text-[20px] mt-14'>Showcase your trading</span>
           <span className='text-white text-[20px]'> skills and risk</span>
           <span className='text-white text-[20px]'>management capabilities</span>
           <span className='text-white text-[20px]'> in a structured evaluation.</span>
         </div>
-        <div className='flex flex-col bg-purple-800 rounded-3xl max-w-[300px] p-4' data-aos="fade-up">
-          <span className='text-black text-[25px] font-bold'>Step 2:</span>
-          <span className='text-black text-[25px] font-bold'>Risk Verification</span>
-          <span className='text-black text-[25px] font-bold'>Verification</span>
+        <div className='flex flex-col bg-purple-800 rounded-3xl max-w-[300px] p-4' data-aos="fade-up" data-aos-delay="1500">
+          <span className='text-white text-[25px] font-bold'>Step 2:</span>
+          <span className='text-white text-[25px] font-bold'>Risk Verification</span>
+          <span className='text-white text-[25px] font-bold'>Verification</span>
           <span className='text-white text-[20px] mt-4'>Verify your understanding</span>
           <span className='text-white text-[20px]'>  of risks involved in trading </span>
           <span className='text-white text-[20px]'> to ensure responsible </span>
           <span className='text-white text-[20px]'> management of capital.</span>
         </div>
 
-        <div className='flex flex-col bg-purple-800 rounded-3xl max-w-[300px] p-4' data-aos="fade-down">
-          <span className='text-black text-[25px] font-bold'>Step 3 :</span>
-          <span className='text-black text-[25px] font-bold'>Monetized Training</span>
-          <span className='text-black text-[25px] font-bold'>Training</span>
+        <div className='flex flex-col bg-purple-800 rounded-3xl max-w-[300px] p-4' data-aos="fade-down" data-aos-delay="1500">
+          <span className='text-white text-[25px] font-bold'>Step 3 :</span>
+          <span className='text-white text-[25px] font-bold'>Monetized Training</span>
+          <span className='text-white text-[25px] font-bold'>Training</span>
           <span className='text-white text-[20px] mt-4'>Start earning real profits</span>
           <span className='text-white text-[20px]'>  while you continue to</span>
           <span className='text-white text-[20px]'> learn. Apply your skills in a</span>
@@ -49,10 +49,10 @@ const Hero2 = () => {
           
         </div>
 
-        <div className='flex flex-col bg-purple-800 rounded-3xl max-w-[300px] p-4' data-aos="fade-up">
-          <span className='text-black text-[25px] font-bold'>Step 4 :</span>
-          <span className='text-black text-[25px] font-bold'>Manage Hedge</span>
-            <span className='text-black text-[25px] font-bold'>Fund Capital</span>
+        <div className='flex flex-col bg-purple-800 rounded-3xl max-w-[300px] p-4' data-aos="fade-up" data-aos-delay="1500">
+          <span className='text-white text-[25px] font-bold'>Step 4 :</span>
+          <span className='text-white text-[25px] font-bold'>Manage Hedge</span>
+            <span className='text-white text-[25px] font-bold'>Fund Capital</span>
           <span className='text-white text-[20px] mt-4 text-center'>Top performers transition</span>
           <span className='text-white text-[20px]'>to managing significant</span>
           <span className='text-white text-[20px]'> assets in an affiliated</span>
